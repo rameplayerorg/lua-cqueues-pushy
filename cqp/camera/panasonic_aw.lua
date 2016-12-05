68cfa10b83a64d11528f69ee7daf1f2c8a1c37b3
Timo Teräs <timo.teras@iki.fi>
2016-12-05 14:32:23 +0200

    panasonic_aw: support getting and going to absolution position

--- cqp/camera/panasonic_aw.lua
+++ cqp/camera/panasonic_aw.lua
@@ -6,6 +6,9 @@ local http = require 'cqp.http'
 local PanasonicAW = {}
 PanasonicAW.__index = PanasonicAW
 
+local sync=2
+local global_seq=1
+
 local function limit(val, _min, _max)
 	if val < _min then return _min end
 	if val > _max then return _max end
@@ -19,7 +22,7 @@ end
 function PanasonicAW:init(ip)
 	self.__ip = ip
 	self.__cmdold = {}
-	self.__cmdqueue = nil
+	self.__cmdqueue = {}
 	self.__cmdcond = condition.new()
 	self.__pan = 50
 	self.__tilt = 50
@@ -39,31 +42,57 @@ function PanasonicAW:init(ip)
 end
 
 function PanasonicAW:aw_ptz(force, cmd, fmt, ...)
-	local val = fmt:format(...)
+	local val = fmt and fmt:format(...) or ""
+	local seq = cmd
 	if force or self.__cmdold[cmd] ~= val then
-		self.__cmdqueue = self.__cmdqueue or {}
-		self.__cmdqueue[cmd] = val
+		local cmdtbl = { cmd = cmd, val = val }
+		if force == sync then
+			cmdtbl.cond = condition.new()
+			seq = global_seq
+			global_seq = global_seq + 1
+		end
+		self.__cmdqueue[seq] = cmdtbl
 		self.__cmdold[cmd] = val
 		self.__cmdcond:signal(1)
+		if force == sync then
+			cmdtbl.cond:wait()
+			return cmdtbl.res
+		end
 	end
 end
 
 function PanasonicAW:goto_preset(no) self:aw_ptz(true, "R", "%02d", no-1) end
 function PanasonicAW:save_preset(no) self:aw_ptz(true, "M", "%02d", no-1) end
 
+function PanasonicAW:get_abs()
+	local pos, zoom
+	pos = self:aw_ptz(sync, "APC")
+	zoom = self:aw_ptz(sync, "GZ")
+	return pos:sub(4) .. zoom:sub(3)
+end
+
+function PanasonicAW:goto_abs(pos)
+	self:aw_ptz(false, "APC", "%08s", pos:sub(1, 8))
+	self:aw_ptz(false, "AXZ", "%03s", pos:sub(9, 11))
+end
+
 function PanasonicAW:main()
 	while true do
 		local sleep = true
-		for cmd, val in pairs(self.__cmdqueue) do
-			local uri = ("/cgi-bin/aw_ptz?cmd=%%23%s%s&res=1"):format(cmd, val)
-			local status = http.get(self.__ip, 80, uri)
+		for seq, val in pairs(self.__cmdqueue) do
+			local uri = ("/cgi-bin/aw_ptz?cmd=%%23%s%s&res=1"):format(val.cmd, val.val)
+			local status, res = http.get(self.__ip, 80, uri)
 			print("Posting", self.__ip, uri, status)
-			if status == 200 and self.__cmdqueue[cmd] == val then
+			if status == 200 and self.__cmdqueue[seq] == val then
 				-- ACK command from queue (unless we got new already)
-				self.__cmdqueue[cmd] = nil
+				self.__cmdqueue[seq] = nil
 			elseif status == nil then
 				-- HTTP failed, throttle resend
 				cqueues.sleep(1.0)
+			end
+			if val.cond then
+				val.res = res
+				val.cond:signal()
 			end
 
 			sleep = false
